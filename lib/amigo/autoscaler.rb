29ffd51058f3195b60cd52a34c3e2f8196c56a27
Rob Galanakis <rob.galanakis@gmail.com>
2023-03-27 23:05:52 -0400

    Add 'latency restored' callback to autoscaler
    `Amigo::Autoscaler` could only really report about high latency;
    when the latency was restored, it couldn't take any action.
    Now there is a `latency_restored_handlers` keyword that allows
    callbacks when the high latency event is resolved.
    
    In order to make autoscaling more useful,
    the 'latency alert' handlers also take a `depth` (number of alerts)
    and `duration` (how long we've been in high latency).
    This allows you to, for example, provision more workers
    up to a certain amount.

--- lib/amigo/autoscaler.rb
+++ lib/amigo/autoscaler.rb
@@ -31,18 +31,26 @@ module Amigo
     # @return [Integer]
     attr_reader :latency_threshold
     # What hosts/processes should this run on?
-    # Look at ENV['DYNO'] and Socket.gethostname.
+    # Looks at ENV['DYNO'] and Socket.gethostname for a match.
     # Default to only run on 'web.1', which is the first Heroku web dyno.
     # We run on the web, not worker, dyno, so we report backed up queues
     # in case we, say, turn off all workers (broken web processes
     # are generally easier to find).
     # @return [Regexp]
     attr_reader :hostname_regex
-    # Methods to call when alerting.
-    # Valid values are 'log' and 'sentry' (requires Sentry to be required already).
-    # Anything that responds to +call+ will be invoked with a hash of
-    # `{queue name => latency in seconds}`.
-    # @return [Array<String,Proc>]
+    # Methods to call when alerting, as strings/symbols or procs.
+    # Valid string values are 'log' and 'sentry' (requires Sentry to be required already).
+    # Anything that responds to +call+ will be invoked with:
+    # - Positional argument which is a +Hash+ of `{queue name => latency in seconds}`
+    # - Keyword argument +:depth+: Number of alerts as part of this latency event.
+    #   For example, the first alert has a depth of 1, and if latency stays high,
+    #   it'll be 2 on the next call, etc. +depth+ can be used to incrementally provision
+    #   additional processing capacity, and stop adding capacity at a certain depth
+    #   to avoid problems with too many workers (like excessive DB load).
+    # - Keyword argument +:duration+: Number of seconds since this latency spike started.
+    # - Additional undefined keywords. Handlers should accept additional options,
+    #   like via `**kw` or `opts={}`, for compatibility.
+    # @return [Array<String,Symbol,Proc,#call>]
     attr_reader :handlers
     # Only alert this often.
     # For example, with poll_interval of 10 seconds
@@ -50,20 +58,50 @@ module Amigo
     # we'd alert once and then 210 seconds later.
     # @return [Integer]
     attr_reader :alert_interval
+    # After an alert happens, what latency should be considered "back to normal" and
+    # +latency_restored_handlers+ will be called?
+    # In most cases this should be the same as (and defaults to) +latency_threshold+
+    # so that we're 'back to normal' once we're below the threshold.
+    # It may also commonly be 0, so that the callback is fired when the queue is entirely clear.
+    # Note that, if +latency_restored_threshold+ is less than +latency_threshold+,
+    # while the latency is between the two, no alerts will fire.
+    attr_reader :latency_restored_threshold
+    # Methods to call when a latency of +latency_restored_threshold+ is reached
+    # (ie, when we get back to normal latency after a high latency event).
+    # Valid string values are 'log'.
+    # Usually this handler will deprovision capacity procured as part of the alert +handlers+.
+    # Anything that responds to +call+ will be invoked with:
+    # - Keyword +:depth+, the number of times an alert happened before
+    #   the latency spike was resolved.
+    # - Keyword +:duration+, the number of seconds for the latency spike has been going on.
+    # - Additional undefined keywords. Handlers should accept additional options,
+    #   like via `**kw`, for compatibility.
+    # @return [Array<String,Symbol,Proc,#call>]
+    attr_reader :latency_restored_handlers
 
     def initialize(
       poll_interval: 20,
       latency_threshold: 5,
       hostname_regex: /^web\.1$/,
-      handlers: ["log"],
-      alert_interval: 120
+      handlers: [:log],
+      alert_interval: 120,
+      latency_restored_threshold: latency_threshold,
+      latency_restored_handlers: [:log]
     )
 
+      raise ArgumentError, "latency_threshold must be > 0" if
+        latency_threshold <= 0
+      raise ArgumentError, "latency_restored_threshold must be >= 0" if
+        latency_restored_threshold.negative?
+      raise ArgumentError, "latency_restored_threshold must be <= latency_threshold" if
+        latency_restored_threshold > latency_threshold
       @poll_interval = poll_interval
       @latency_threshold = latency_threshold
       @hostname_regex = hostname_regex
-      @handlers = handlers
+      @handlers = handlers.freeze
       @alert_interval = alert_interval
+      @latency_restored_threshold = latency_restored_threshold
+      @latency_restored_handlers = latency_restored_handlers.freeze
     end
 
     def polling_thread
@@ -73,17 +111,18 @@ module Amigo
     def setup
       # Store these as strings OR procs, rather than grabbing self.method here.
       # It gets extremely hard ot test if we capture the method here.
-      @alert_methods = self.handlers.map do |a|
-        if a.respond_to?(:call)
-          a
-        else
-          method_name = meth = "alert_#{a.strip}".to_sym
-          raise InvalidHandler, a.inspect unless self.method(method_name)
-          meth
-        end
-      end
+      @alert_methods = self.handlers.map { |a| _handler_to_method("alert_", a) }
+      @restored_methods = self.latency_restored_handlers.map { |a| _handler_to_method("alert_restored_", a) }
       @last_alerted = Time.at(0)
       @stop = false
+      @depth = 0
+    end
+
+    private def _handler_to_method(prefix, a)
+      return a if a.respond_to?(:call)
+      method_name = "#{prefix}#{a.to_s.strip}".to_sym
+      raise InvalidHandler, a.inspect unless (meth = self.method(method_name))
+      return meth
     end
 
     def start
@@ -119,9 +158,37 @@ module Amigo
         map { |q| [q.name, q.latency] }.
         select { |(_, latency)| latency > self.latency_threshold }.
         to_h
-      return if high_latency_queues.empty?
+      if high_latency_queues.empty?
+        # Whenever we are in a latency event, we have a depth > 0. So a depth of 0 means
+        # we're not in a latency event, and still have no latency, so can noop.
+        return if @depth.zero?
+        # We WERE in a latency event, and now we're not, so report on it.
+        @restored_methods.each do |m|
+          m.call(depth: @depth, duration: (Time.now - @latency_event_started).to_f)
+        end
+        # Reset back to 0 depth so we know we're not in a latency event.
+        @depth = 0
+        return
+      end
+      if @depth.positive?
+        # We have already alerted, so increment the depth and when the latency started.
+        @depth += 1
+        duration = (Time.now - @latency_event_started).to_f
+      else
+        # Indicate we are starting a high latency event.
+        @depth = 1
+        @latency_event_started = Time.now
+        duration = 0.0
+      end
+      # Alert each handler. For legacy reasons, we support handlers that accept
+      # ({queues and latencies}) and ({queues and latencies}, {}keywords}).
+      kw = {depth: @depth, duration: duration}
       @alert_methods.each do |m|
-        m.respond_to?(:call) ? m.call(high_latency_queues) : self.send(m, high_latency_queues)
+        if m.respond_to?(:arity) && m.arity == 1
+          m.call(high_latency_queues)
+        else
+          m.call(high_latency_queues, **kw)
+        end
       end
       @last_alerted = now
     end
@@ -134,11 +201,15 @@ module Amigo
       end
     end
 
-    def alert_log(names_and_latencies)
-      self.log(:warn, "high_latency_queues", queues: names_and_latencies)
+    def alert_log(names_and_latencies, depth:, duration:)
+      self.log(:warn, "high_latency_queues", queues: names_and_latencies, depth: depth, duration: duration)
     end
 
-    def alert_test(_names_and_latencies); end
+    def alert_test(_names_and_latencies, _opts={}); end
+
+    def alert_restored_log(depth:, duration:)
+      self.log(:info, "high_latency_queues_restored", depth: depth, duration: duration)
+    end
 
     protected def log(level, msg, **kw)
       Amigo.log(nil, level, msg, kw)
--- spec/amigo/autoscaler_spec.rb
+++ spec/amigo/autoscaler_spec.rb
@@ -26,6 +26,31 @@ RSpec.describe Amigo::Autoscaler do
     return cls.new
   end
 
+  describe "initialize" do
+    it "errors for a negative or 0 latency_threshold" do
+      expect do
+        described_class.new(latency_threshold: -1)
+      end.to raise_error(ArgumentError)
+    end
+
+    it "errors for a negative latency_restored_threshold" do
+      expect do
+        described_class.new(latency_restored_threshold: -1)
+      end.to raise_error(ArgumentError)
+    end
+
+    it "errors if the latency restored threshold is > latency threshold" do
+      expect do
+        described_class.new(latency_threshold: 100, latency_restored_threshold: 101)
+      end.to raise_error(ArgumentError)
+    end
+
+    it "defaults latency restored threshold to latency threshold" do
+      x = described_class.new(latency_threshold: 100)
+      expect(x).to have_attributes(latency_restored_threshold: 100)
+    end
+  end
+
   describe "start" do
     it "starts a polling thread if the dyno env var matches the given regex" do
       allow(Sidekiq::Queue).to receive(:all).and_return([fake_q("x", 0)])
@@ -70,9 +95,50 @@ RSpec.describe Amigo::Autoscaler do
     it "alerts about high latency queues" do
       expect(Sidekiq::Queue).to receive(:all).and_return([fake_q("x", 1), fake_q("y", 20)])
       o = instance
-      expect(o).to receive(:alert_test).with({"y" => 20})
+      expect(o).to receive(:alert_test).with({"y" => 20}, duration: 0, depth: 1)
       o.setup
       o.check
+    end
+
+    it "keeps track of duration and depth after multiple alerts" do
+      expect(Sidekiq::Queue).to receive(:all).twice.and_return([fake_q("y", 20)])
+      o = instance(alert_interval: 0)
+      expect(o).to receive(:alert_test).with({"y" => 20}, duration: 0, depth: 1)
+      o.setup
+      o.check
+      sleep 0.1
+      expect(o).to receive(:alert_test).with({"y" => 20}, duration: be > 0.05, depth: 2)
+      o.check
+    end
+
+    it "alerts with keywords when handlers have keyword (2) arity" do
+      expect(Sidekiq::Queue).to receive(:all).and_return([fake_q("y", 20)])
+      got = []
+      handler = ->(q, kw) { got << [q, kw] }
+      o = instance(handlers: [handler])
+      o.setup
+      o.check
+      expect(got).to eq([[{"y" => 20}, {depth: 1, duration: 0.0}]])
+    end
+
+    it "alerts with keywords when handlers have splat arity" do
+      expect(Sidekiq::Queue).to receive(:all).and_return([fake_q("y", 20)])
+      got = []
+      handler = proc { |*a| got << a }
+      o = instance(handlers: [handler])
+      o.setup
+      o.check
+      expect(got).to eq([[{"y" => 20}, {depth: 1, duration: 0.0}]])
+    end
+
+    it "alerts without depth when handlers have no keyword (1) arity" do
+      expect(Sidekiq::Queue).to receive(:all).and_return([fake_q("y", 20)])
+      got = []
+      handler = ->(q) { got << q }
+      o = instance(handlers: [handler])
+      o.setup
+      o.check
+      expect(got).to eq([{"y" => 20}])
     end
 
     it "noops if recently alerted" do
@@ -81,11 +147,24 @@ RSpec.describe Amigo::Autoscaler do
         and_return([fake_q("x", 1), fake_q("y", 20)])
       now = Time.now
       o = instance(alert_interval: 120)
-      o.setup
       expect(o).to receive(:alert_test).twice
+      o.setup
       Timecop.freeze(now) { o.check }
       Timecop.freeze(now + 60) { o.check }
       Timecop.freeze(now + 180) { o.check }
+    end
+
+    it "invokes latency restored handlers once all queues have a latency at/below the threshold" do
+      expect(Sidekiq::Queue).to receive(:all).
+        and_return([fake_q("y", 20)], [fake_q("y", 3)], [fake_q("y", 2)])
+      o = instance(alert_interval: 0, latency_threshold: 2)
+      expect(o).to receive(:alert_test).with({"y" => 20}, duration: be_a(Float), depth: 1)
+      expect(o).to receive(:alert_test).with({"y" => 3}, duration: be_a(Float), depth: 2)
+      expect(o).to receive(:alert_restored_log).with(duration: be_a(Float), depth: 2)
+      o.setup
+      o.check
+      o.check
+      o.check
     end
   end
 
@@ -97,8 +176,8 @@ RSpec.describe Amigo::Autoscaler do
     it "logs" do
       Amigo.structured_logging = true
       expect(Amigo.log_callback).to receive(:[]).
-        with(nil, :warn, "high_latency_queues", {queues: {"x" => 11, "y" => 24}})
-      instance.alert_log({"x" => 11, "y" => 24})
+        with(nil, :warn, "high_latency_queues", {queues: {"x" => 11, "y" => 24}, depth: 5, duration: 20.5})
+      instance.alert_log({"x" => 11, "y" => 24}, depth: 5, duration: 20.5)
     end
   end
 
@@ -137,6 +216,19 @@ RSpec.describe Amigo::Autoscaler do
       o.setup
       o.check
       expect(called_with).to eq({"y" => 20})
+    end
+  end
+
+  describe "alert_restored_log" do
+    after(:each) do
+      Amigo.reset_logging
+    end
+
+    it "logs" do
+      Amigo.structured_logging = true
+      expect(Amigo.log_callback).to receive(:[]).
+        with(nil, :info, "high_latency_queues_restored", {depth: 2, duration: 10.5})
+      instance.alert_restored_log(depth: 2, duration: 10.5)
     end
   end
 end
